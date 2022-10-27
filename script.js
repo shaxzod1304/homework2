2914afb0f43dbf52605c2148f88cd4d74f453bc0
Shaxzod <tursunmuratoshaxzod@gmail.com>
2022-10-27 22:57:30 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+
+// let qname = prompt('Как тебя зовут')
+
+
+// if (name === 'Alex') {
+    
+// } else(
+//     ('Ты мне не знаком')
+// )
+
+// let account = prompt('Номер карты')
+
+
+
+//2
+
+let name = prompt('what is your name?').toLowerCase()
+let age = prompt('how old are you?')
+let money = prompt('how much money do you have,')
+
+if(age >= 20 && age <= 40 && name[0] === "a" && money >= "100$" ) {
+    console.log("WELCOME BROO");
+} else {
+    console.log("go home");
+}
